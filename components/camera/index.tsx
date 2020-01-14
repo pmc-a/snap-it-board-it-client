cf08a48f3e71ed0ec60bf054cd6c83193082acfa
Peter McAree <pmcaree08@gmail.com>
2020-01-14 19:34:16 +0000

    add: initial camera support with basic navigation between views

---
+++ components/camera/index.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+
+interface IProps {
+	setShowCamera: Function;
+}
+
+const CustomCamera: React.FunctionComponent<IProps> = ({
+	setShowCamera,
+}) => {
+	let camera: any = useRef(null);
+
+	const [hasPermission, setHasPermission] = useState(null);
+	
+	useEffect(() => {
+		(async () => {
+		  const { status } = await Camera.requestPermissionsAsync();
+		  setHasPermission(status === 'granted');
+		})();
+	}, []);
+	
+	if (hasPermission === null) {
+		return <View />;
+	}
+	if (hasPermission === false) {
+		return <Text>No access to camera</Text>;
+	}
+
+	const snapPhoto = async () => {
+		if (camera) {
+			let photo = await camera.current.takePictureAsync();
+			// TODO: Save to camera roll
+			// TODO: Insert HTTP request to send this to backend
+			// Example URI: file:///var/mobile/Containers/Data/Application/921AC448-CC83-4D9C-A94E-3E17B6588E92/Library/Caches/ExponentExperienceData/%2540anonymous%252Fsnap-it-board-it-client-f24edca6-61fd-49cb-b111-bde940eb6484/Camera/2AAD9EA9-31CA-480A-8F17-5807C8957BBF.jpg
+			console.log(photo);
+		}
+	}
+
+	return (
+		<View style={{ flex: 1 }}>
+			<Camera style={{ flex: 1 }} type={Camera.Constants.Type.back} ref={camera}>
+				<View
+				style={{
+					flex: 1,
+					backgroundColor: 'transparent',
+					flexDirection: 'row',
+				}}>
+					<TouchableOpacity
+						style={{
+						flex: 0.25,
+						alignSelf: 'center',
+						alignItems: 'center',
+						}}
+						onPress={() => setShowCamera(false)}>
+						<Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Back </Text>
+					</TouchableOpacity>
+
+					<TouchableOpacity
+						style={{
+						flex: 1,
+						alignSelf: 'center',
+						alignItems: 'center',
+						}}
+						onPress={() => snapPhoto()}>
+						<Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Take Photo </Text>
+					</TouchableOpacity>
+				</View>
+			</Camera>
+		</View>
+	)
+};
+
+export default CustomCamera;
